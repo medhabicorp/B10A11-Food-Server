94e52a533bc117e32df156c4048ccab24311adcb
MdRezaulHoq <muradsust3@gmail.com>
2025-02-16 00:55:25 +0600

    firebase frontend link added in cors

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.use(
           "http://localhost:5173",
           "http://localhost:5174",
           "http://localhost:5175",
+          "https://b10a11-food.web.app",
+          "https://b10a11-food.firebaseapp.com",
       ],
       credentials: true,
   })
