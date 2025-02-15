50ce168cdebb92037fbde555561f371751558edb
MdRezaulHoq <muradsust3@gmail.com>
2025-02-15 14:58:49 +0600

    Fetch all foods and featured food fucntion done

--- index.js
+++ index.js
@@ -87,7 +87,74 @@ async function run() {
         .send({ success: true })
 });
 
+// get all Foods
+app.get("/all-foods", async (req, res) => {
+  const { available, search, sort } = req.query;
 
+  let query = {};
+
+  if (available) {
+    query.status = "Available";
+  }
+
+  if (search) {
+    query.foodName = { $regex: search, $options: "i" };
+  }
+
+  try {
+    const foods = await foodCollection.find(query).toArray();
+    if (sort === "asc") {
+      foods.sort((a, b) =>
+        compareAsc(new Date(a.expireDate), new Date(b.expireDate))
+      );
+    } else if (sort === "dsc") {
+      foods.sort((a, b) =>
+        compareDesc(new Date(a.expireDate), new Date(b.expireDate))
+      );
+    }
+
+    res.send(foods);
+  } catch (error) {
+    console.error("Error fetching foods:", error);
+    res.status(500).send({ message: "Error fetching foods" });
+  }
+});
+
+//get all food data from Database
+app.get("/foods", async (req, res) => {
+  const foods = await foodCollection.find().toArray();
+  res.send(foods);
+});
+
+//get Featured food data databases by quantity
+app.get("/featured-foods", async (req, res) => {
+  const foods = await foodCollection
+    .find({ status: "Available" })
+    .sort({ foodQuantity: -1 })
+    .limit(6)
+    .toArray();
+  res.send(foods);
+});
+
+// get requested food by login user email
+app.get("/request-foods", verifyToken, async (req, res) => {
+  const email = req.query.email;
+  if (req.user.email !== email) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
+  const result = await requestedFoodCollection
+    .find({ user_email: email })
+    .toArray();
+  res.send(result);
+});
+
+//get single food database use id
+app.get("/all-foods/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await foodCollection.findOne(query);
+  res.send(result);
+});
 
 //post all add foods
 app.post("/all-foods", verifyToken, async (req, res) => {
@@ -96,6 +163,64 @@ app.post("/all-foods", verifyToken, async (req, res) => {
   res.send(result);
 });
 
+
+//post request foods
+app.post("/request-foods", verifyToken, async (req, res) => {
+  const foodRequest = req.body;
+  if (req.user.email !== foodRequest.user_email) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  const foodId = foodRequest.food_id;
+  const result = await requestedFoodCollection.insertOne(foodRequest);
+  const filter = { _id: new ObjectId(foodId) };
+  const updateDoc = {
+    $set: { status: foodRequest.status },
+  };
+  const updateResult = await foodCollection.updateOne(filter, updateDoc);
+  res.send(result);
+});
+
+
+// manage my foods by login email
+app.get("/manage-my-foods", verifyToken, async (req, res) => {
+  const email = req.query.email;
+  if (req.user.email !== email) {
+    return res.status(403).send({ message: "Forbidden" });
+  }
+  const query = { "donator.donatorEmail": email };
+  const result = await foodCollection.find(query).sort({ _id: -1 }).toArray();
+  res.send(result);
+});
+
+
+app.patch("/all-foods/:id", verifyToken, async (req, res) => {
+  const food = req.body;
+  const id = req.params.id;
+  console.log("updated", id);
+  console.log("verified email", req.user.email);
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      foodName: food.foodName,
+      foodImg: food.foodImg,
+      foodQuantity: food.foodQuantity,
+      location: food.location,
+      expireDate: food.expireDate,
+      additionalNotes: food.additionalNotes,
+    },
+  };
+  const result = await foodCollection.updateOne(filter, updateDoc);
+  res.send(result);
+});
+
+
+//delete all food databases
+app.delete("/all-foods/:id", verifyToken, async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const result = await foodCollection.deleteOne(filter);
+  res.send(result);
+});
 
   } finally {
     // Ensures that the client will close when you finish/error
